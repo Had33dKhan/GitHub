c59b9ed7c2fec7660e5e403d32c6617f0d86fb26
Muhammad Hadeed Khan <166932393+Had33dKhan@users.noreply.github.com>
2024-06-01 12:11:52 +0500

    Initial commit

---
+++ app/src/main/java/com/example/github/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.github
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webview = findViewById<WebView>(R.id.WebView)
+        webViewSetup(webview)
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun webViewSetup(webView: WebView){
+        webView.webViewClient = WebViewClient()
+
+        webView.apply {
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled=true
+            loadUrl("https://github.com/Had33dKhan")
+        }
+    }
+}
